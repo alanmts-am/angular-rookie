1661dbca56fc1b9c8e24d9ac51ed4ce7e04b0ca4
Alan Mateus <76778046+alanmts-am@users.noreply.github.com>
2024-08-18 23:00:01 -0300

    feat: add base structure to ngSwitch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,8 @@ export class HomeComponent {
   nomeIo = "Esse texto está no home mas sendo levado até o io-properties"
   valorRecebido!: number;
 
+  nav = 'home';
+
   onMudouValor(evento: any) {
     this.valorRecebido = evento.novoValor
   }
